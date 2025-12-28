2206b078d15bf89dd2b167b74b70b37fd2f6e926
Haha64142 <paul.w64142@gmail.com>
2025-12-28 17:30:20 -0600

    Add prettier formatting for js, html, and css

--- init.lua
+++ init.lua
@@ -784,6 +784,9 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        javascript = { 'prettier' },
+        html = { 'prettier' },
+        css = {  'prettier' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
