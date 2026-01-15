a2b5ef021f90568e34d30a11722182ad0d2881f1
Haha64142 <paul.w64142@gmail.com>
2026-01-15 16:44:25 -0600

    Update html/js/css formatters to use prettierd first

--- init.lua
+++ init.lua
@@ -784,9 +784,9 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        javascript = { 'prettier' },
-        html = { 'prettier' },
-        css = {  'prettier' },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
+        html = { 'prettierd', 'prettier', stop_after_first = true },
+        css = { 'prettierd', 'prettier', stop_after_first = true },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
